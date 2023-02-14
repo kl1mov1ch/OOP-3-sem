08604eca4c686f952343b5197af3a91f00c719c9
kl1mov1ch <87700609+kl1mov1ch@users.noreply.github.com>
2023-02-14 18:54:48 +0300

    Add files via upload

---
+++ LAB17-18/Lab_17-18_OOP/Lab_17-18_OOP/Subscriber.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_17_18_OOP
+{
+    // класс абстрактной фабрики
+    public abstract class SubsriberFactory
+    {
+        public abstract string CreateService();
+    }
+    // Фабрика создания абонента 1
+    public class Subsriber3GFactory : SubsriberFactory
+    {
+        public override string CreateService()
+        {
+            return "3G";
+        }
+    }
+    // Фабрика создания абонента 2
+    public class Subsriber4GFactory : SubsriberFactory
+    {
+        public override string CreateService()
+        {
+            return "4G";
+        }
+    }
+    // клиент
+    public class Subsriber
+    {
+        private List<Service> services = new List<Service>();
+        private string typeWIC;
+        private Bill bill;
+        private int _id;
+        public Subsriber(SubsriberFactory factory, int id)
+        {
+            bill = new Bill();
+            bill.summa = 0;
+            typeWIC = factory.CreateService();
+            _id = id;
+        }
+        public void AddService(Service service)
+        {
+            services.Add(service);
+            bill.summa += service.total;
+        }
+        public int GetBill()
+        {
+            return bill.summa;
+        }
+        public void UseServices()
+        {
+            Console.WriteLine($"Абонент #{_id}, типа {typeWIC} на связи");
+            if (services != null)
+                foreach (Service service in services)
+                    service.DoService();
+            else
+                Console.WriteLine("Услуги не подключены");
+        }
+        public override string ToString()
+        {
+            return Convert.ToString(_id);
+        }
+    }
+}
