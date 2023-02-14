eecd838d2f47ef9af55bb4f26efb6340e25f1a7e
kl1mov1ch <87700609+kl1mov1ch@users.noreply.github.com>
2023-02-14 18:30:40 +0300

    Add files via upload

---
+++ LAB7/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.IO;
+
+namespace Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Set<int> set1 = new Set<int>(),
+            set2 = new Set<int>(),
+            set3 = new Set<int>();
+            Set<string> set4 = new Set<string>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                string str = "Qwerty";
+                set1.Add(i + 1);
+                set1.Add(i + 10);
+                set2.Add(i + 4);
+                set3.Add(i + 15);
+                set4.Add(i + str);
+            }
+            Console.Write("Множество один: ");
+            foreach (var item in set1)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.Write("\nМножество два: ");
+            foreach (var item in set2)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.Write("\nМножество три: ");
+            foreach (var item in set3)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.Write("\nМножество четыре: ");
+            foreach (var item in set4)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.Write("\nМножество четыре: ");
+            foreach (var item in set4)
+            {
+                Console.Write(item + " ");
+            }
+            Set<int>.StaticOperation.DeleteNullElements(ref set4);
+            Console.Write("\nМножество четыре без нулевых элементов: ");
+            foreach (var item in set4)
+            {
+                Console.Write(item + " ");
+            }
+            Set<int>.StaticOperation.Point(ref set4);
+            Console.Write("\nМножество четыре: ");
+            foreach (var item in set4)
+            {
+                Console.Write(item + " ");
+            }
+
+            Set<int>.Developer developer = new Set<int>.Developer();
+            Set<int>.Production production = new Set<int>.Production();
+            Console.WriteLine();
+            Console.WriteLine(developer.ToString());
+            Console.WriteLine(production.ToString());
+            try
+            {
+            IUser<int> user1 = new User<int>(1337);
+            Console.WriteLine(user1.GetUserID());
+
+            IUser<string> user2 = new User<string>("Hi");
+            Console.WriteLine(user2.GetUserID());
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                Console.WriteLine("Bomj");
+            }
+            try
+            {
+                string line = set1.ToString();
+                
+                StreamReader sr = new StreamReader(@"C:\\Sample.txt");
+                
+                line = sr.ReadLine();
+                
+                while (line != null)
+                {
+                   
+                    Console.WriteLine(line);
+                   
+                    line = sr.ReadLine();
+                }
+               
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+
+       
+        interface IUser<T>
+        {
+            T GetUserID();
+        }
+
+        class User<T> : IUser<T>
+        {
+            protected T UserID;
+
+            public User(T userID) => UserID = userID;
+
+            public virtual T GetUserID()
+            {
+                return UserID;
+            }
+        }
+
+        interface IMessage
+        {
+            string Text { get; }
+        }
+        interface IPrintable
+        {
+            void Print();
+        }
+        class Message : IMessage, IPrintable
+        {
+            public string Text { get; }
+            public Message(string text) => Text = text;
+
+            public void Print() => Console.WriteLine(Text);
+        }
+
+        class Messenger<T> where T : IMessage, IPrintable
+        {
+            public void Send(T message)
+            {
+                Console.WriteLine("Отправка сообщения:");
+                message.Print();
+            }
+        }
+    }
+
+}
---
+++ LAB7/Set.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+namespace Set
+{
+    public partial class Set<T> : IEnumerable
+    {
+
+        //Коллекция хранимых данных
+        List<T> _items = new List<T>();
+
+        //Количество элементов
+        public int Count => _items.Count;
+
+        //Добавление данных во множество
+
+        public void Add(T item)
+        {
+            //Проверка вхлоных двнных на нулевой элемент
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+            //Множество может содержать только уникальные элементы
+            //Если множество уже содержит такое  элемент, то не добавляем его
+            if (!_items.Contains(item))
+            {
+                _items.Add(item);
+            }
+        }
+
+        public void Remove(T item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            if (!_items.Contains(item))
+            {
+                throw new KeyNotFoundException($"Элемента {item} нет в этом множестве");
+            }
+            _items.Remove(item);
+        }
+
+        public static Set<T> Union(Set<T> set1, Set<T> set2)
+        {
+            if (set1 == null)
+            {
+                throw new ArgumentNullException(nameof(set1));
+            }
+
+            if (set2 == null)
+            {
+                throw new ArgumentNullException(nameof(set1));
+            }
+
+            //Результирующее множество
+            var resultSet = new Set<T>();
+            // Элемент данных результирующего множества
+            var items = new List<T>();
+
+            //Если первое входное множество содержит данные, то добавим их в результирующее множество 
+            if (set1._items != null && set2._items.Count > 0)
+            {
+                items.AddRange(new List<T>(set1._items));
+            }
+
+            if (set2._items != null && set2._items.Count > 0)
+            {
+                items.AddRange(new List<T>(set2._items));
+            }
+
+            resultSet._items = items.Distinct().ToList();
+
+            return resultSet;
+
+        }
+
+
+        public static Set<T> Intersection(Set<T> set1, Set<T> set2)
+        {
+            if (set1 == null)
+            {
+                throw new ArgumentNullException(nameof(set1));
+            }
+
+            if (set2 == null)
+            {
+                throw new ArgumentNullException(nameof(set1));
+            }
+
+            //Результирующее множество
+            var resultSet = new Set<T>();
+
+            if (set1.Count < set2.Count)
+            {
+                foreach (var item in set1._items)
+                {
+                    //Если элемент первого множества содержится во втором, то добавляем его в резутьтирующее множество
+                    if (set2._items.Contains(item))
+                    {
+                        resultSet.Add(item);
+                    }
+                }
+            }
+            else
+            {
+                //Если элемент второго множества содержится в первом, то добавляем его в резутьтирующее множество
+                foreach (var item in set2._items)
+                {
+                    if (set1._items.Contains(item))
+                    {
+                        resultSet.Add(item);
+                    }
+                }
+            }
+            return resultSet;
+        }
+
+        public static Set<T> Difference(Set<T> set1, Set<T> set2)
+        {
+            if (set1 == null)
+            {
+                throw new ArgumentNullException(nameof(set1));
+            }
+
+            if (set2 == null)
+            {
+                throw new ArgumentNullException(nameof(set1));
+            }
+
+            var resultSet = new Set<T>();
+
+            foreach (var item in set1._items)
+            {
+                if (!set2._items.Contains(item))
+                {
+                    resultSet.Add(item);
+                }
+            }
+
+            foreach (var item in set2._items)
+            {
+                if (!set1._items.Contains(item))
+                {
+                    resultSet.Add(item);
+                }
+            }
+
+            resultSet._items = resultSet._items.Distinct().ToList();
+
+            return resultSet;
+        }
+
+        public static bool Subset(Set<T> set1, Set<T> set2)
+        {
+            if (set1 == null)
+            {
+                throw new ArgumentNullException(nameof(set1));
+            }
+
+            if (set2 == null)
+            {
+                throw new ArgumentNullException(nameof(set1));
+            }
+
+            var result = set1._items.All(s => set2._items.Contains(s));
+            return result;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+
+        public static Set<T> operator +(Set<T> set1, Set<T> set2)
+        {
+            return Union(set1, set2);
+        }
+
+        public static bool operator <(Set<T> set1, Set<T> set2)
+        {
+            return Subset(set1, set2);
+        }
+        public static bool operator >(Set<T> set1, Set<T> set2)
+        {
+            return Subset(set1, set2);
+        }
+
+        public static Set<T> operator -(Set<T> set, T item)
+        {
+            set.Remove(item);
+            return set;
+        }
+
+        public static Set<T> operator *(Set<T> set1, Set<T> set2)
+        {
+            return Intersection(set1, set2);
+        }
+    }
+    public partial class Set<T>
+    {
+        public class Production
+        {
+            static string nameOrganization = "БГТУ";
+            int id;
+            static int counter;
+
+            static Production()
+            {
+                counter = 0;
+            }
+
+            public Production()
+            {
+                counter++;
+                id = counter;
+            }
+
+            public override string ToString()
+            {
+                return nameOrganization;
+            }
+        }
+
+        public class Developer
+        {
+            static string nameDeveloper = "Антон";
+            int id;
+            static int counter;
+
+            static Developer()
+            {
+                counter = 0;
+            }
+
+            public Developer()
+            {
+                counter++;
+                id = counter;
+            }
+
+            public override string ToString()
+            {
+                return nameDeveloper;
+            }
+        }
+
+        public static class StaticOperation
+        {
+            public static void Point(ref Set<string> set)
+            {
+                for (int i = 0; i < set._items.Count; i++)
+                {
+                    set._items[i] += ".";
+                }
+            }
+
+            public static void DeleteNullElements(ref Set<string> set)
+            {
+                for (int i = 0; i < set._items.Count; i++)
+                {
+                    if (set._items[i] == "0")
+                    {
+                        set._items[i].Remove(i);
+                        i--;
+                    }
+                }
+            }
+        }
+
+
+    }
+}
